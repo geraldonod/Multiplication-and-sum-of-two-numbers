f5f354fe7c7ec5f874b86c5f7d748ea20466fe46
Gerald <geraldonod@gmail.com>
2024-01-24 08:56:19 +0800

    if else statement

--- Multiplication and Sum of Two Numbers.py
+++ Multiplication and Sum of Two Numbers.py
@@ -3,7 +3,11 @@ def product_or_sum(num1, num2):
     product = num1 * num2
 
 #create if-else statement for sum or product
-
+    if product <= 1000:
+        return product
+    else:
+        return num1 + num2 
+    
 #ask user for input
 
 #print out result
